f6195cbae522cc2aaed22644a4a92893def748e7
xuhao <whitecute.xu@outlook.com>
2020-02-07 17:26:51 +0800

    fix: 预案指导分级政策

---
+++ src/components/home/service-manage/community-triage/community-triage.ts
@@ -0,0 +1,14 @@
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+import CommunityTriageStyle from './community-triage.module.scss';
+import CommunityTriageHtml from './community-triage.html';
+
+@Component({
+  template: CommunityTriageHtml,
+  style: CommunityTriageStyle,
+  themes: [{ name: 'white', style: CommunityTriageStyle }],
+  components: {}
+})
+export class CommunityTriageComponent extends Vue {
+  private activeName = 'first';
+  
+}
--- src/components/home/service-manage/service-manage.ts
+++ src/components/home/service-manage/service-manage.ts
@@ -2,12 +2,16 @@ import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 import serviceManageStyle from './service-manage.module.scss';
 import serviceManageHtml from './service-manage.html';
 
+import { CommunityTriageComponent } from '@/components/home/service-manage/community-triage/community-triage';
 @Component({
   template: serviceManageHtml,
   style: serviceManageStyle,
   themes: [{ name: 'white', style: serviceManageStyle }],
-  components: {}
+  components: {
+    CommunityTriageComponent
+  }
 })
 export class ServiceManageComponent extends Vue {
+  private activeName = 'first';
 
 }
