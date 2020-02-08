fa086576d31d1d0c74ab42a343755a6a36bf3594
limu <1623653684@qq.com>
2020-02-08 18:28:30 +0800

    fix: 小区二维码管理页面

--- src/components/system-manage/community-qr-manage/community-qr-manage.ts
+++ src/components/system-manage/community-qr-manage/community-qr-manage.ts
@@ -1,6 +1,7 @@
 import { Vue, Component } from 'vue-property-decorator';
 import communityQRManageHtml from './community-qr-manage.html';
 import communityQRManageStyle from './community-qr-manage.module.scss';
+import dDataSourceService from '@/api/data-source/d-data-source.service';
 
 @Component({
   template: communityQRManageHtml,
@@ -8,5 +9,60 @@ import communityQRManageStyle from './community-qr-manage.module.scss';
   components: {}
 })
 export class CommunityQRManageComponent extends Vue {
+  // 模糊查询g
+  keywords: any = '';
+  tableData: any = [];
+  multipleSelection: any = [];
+    // 社区信息
+    communityInformation: any = [];
+    // 当前社区id
+    currentCommunityId: any = 'a2e01f0e-6c86-4a41-bcf3-c07c1ffa2f82';
+    // 管辖区域
+    manageArea: any = [];
+    created() {
+      this.getCommunityInformationById(this.currentCommunityId);
+    }
+  /**
+   * 批量生成二维码
+   */
+  batchGenerationQRCode() {}
 
+  toggleSelection(rows: any) {
+    const multipleTableRef: any = this.$refs.multipleTable;
+    if (rows) {
+      rows.forEach((row: any) => {
+        multipleTableRef.toggleRowSelection(row);
+      });
+    } else {
+      multipleTableRef.clearSelection();
+    }
+  }
+
+  handleSelectionChange(val: any) {
+    this.multipleSelection = val;
+  }
+  /**
+   * 生成二维码
+   */
+  generateQRCode() {}
+  /**
+   * 导出信息
+   */
+  exportInfo() {
+
+  }
+    /**
+   * 根据id获取社区信息
+   * @param id
+   *
+   */
+  async getCommunityInformationById(id: any) {
+    const res = await dDataSourceService.findDDataSourceByDataSourceId(id);
+    if (res) {
+      this.communityInformation = res;
+    } else {
+      this.communityInformation = [];
+    }
+    this.tableData = this.communityInformation;
+  }
 }
