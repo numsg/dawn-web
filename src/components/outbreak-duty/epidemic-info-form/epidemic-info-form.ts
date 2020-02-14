d902d7a4939c4412119978f1bf5f2a3306861545
zhaodang <13296548062@163.com>
2020-02-14 17:50:53 +0800

    fix: 删除无用的图片

--- src/components/outbreak-duty/epidemic-info-form/epidemic-info-form.ts
+++ src/components/outbreak-duty/epidemic-info-form/epidemic-info-form.ts
@@ -20,19 +20,23 @@ export class EpidemicInfoFormComponent extends Vue {
 
   curEpidemicPerson: EpidemicPerson = new EpidemicPerson();
 
-  // rules = {
-  //   name: [
-  //     { required: true, message: '请输入姓名', trigger: 'blur' }
-  //     // { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
-  //   ],
-  //   gender: [{ required: true, message: '请选择性别', trigger: 'change' }],
-  //   address: [{ required: true, message: '请输入地址', trigger: 'change' }],
-  //   district: [{ required: true, message: '请输入行政区划', trigger: 'change' }],
-  //   // medicalCondition: [{ required: true, message: '请输入医疗情况', trigger: 'change' }],
-  //   submitTime: [{ type: 'date', required: true, message: '请选择报送时间', trigger: 'change' }]
-  //   // diseaseTime: [{ type: 'date', required: true, message: '请选择发病时间', trigger: 'change' }]
-  // };
+   // 本社区小区
+   @Getter('baseData_communities')
+   communities!: any[];
+   // 确诊情况
+   @Getter('baseData_diagnosisSituations')
+   diagnosisSituations!: any[];
+   // 医疗情况
+   @Getter('baseData_medicalSituations')
+   medicalSituations!: any[];
+   // 特殊情况
+   @Getter('baseData_specialSituations')
+   specialSituations!: any[];
+   // 性别
+   @Getter('baseData_genderClassification')
+   genderClassification!: any[];
 
+   isEdit: boolean = false;
   rules = {
     // code: [{ required: true, message: '请输入编号', trigger: 'blur' }],
     name: [{ required: true, message: '请输入姓名', trigger: ['blur', 'change'] }],
@@ -88,24 +92,6 @@ export class EpidemicInfoFormComponent extends Vue {
       callback();
     }
   }
-
-  // 本社区小区
-  @Getter('baseData_communities')
-  communities!: any[];
-  // 确诊情况
-  @Getter('baseData_diagnosisSituations')
-  diagnosisSituations!: any[];
-  // 医疗情况
-  @Getter('baseData_medicalSituations')
-  medicalSituations!: any[];
-  // 特殊情况
-  @Getter('baseData_specialSituations')
-  specialSituations!: any[];
-  // 性别
-  @Getter('baseData_genderClassification')
-  genderClassification!: any[];
-
-  isEdit: boolean = false;
 
   submitForm(formName: string) {
     const form: any = this.$refs[formName];
